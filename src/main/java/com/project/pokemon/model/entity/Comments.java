1815ee85c245339396324fd7f13174c8374221b6
KorOnechild <kyuhong99@gmail.com>
2022-06-11 12:51:15 +0900

    Comments 엔티티 클래스 작성

--- src/main/java/com/project/pokemon/model/entity/Comments.java
+++ src/main/java/com/project/pokemon/model/entity/Comments.java
@@ -1,4 +1,22 @@
 package com.project.pokemon.model.entity;
 
-public class Comments {
+import javax.persistence.*;
+
+@Entity
+public class Comments extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false)
+    private String comments;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private Users users;
+
+    @ManyToOne
+    @JoinColumn(name = "pokemonId", nullable = false)
+    private Pokemon pokemon;
 }
