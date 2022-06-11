e19fe1d1a50b56168616f5650d60269487bc99c8
MinjuKim <kpoong02@naver.com>
2022-06-11 19:01:41 +0900

    userservice

--- src/main/java/com/project/pokemon/Service/UserService.java
+++ src/main/java/com/project/pokemon/Service/UserService.java
@@ -1,4 +1,31 @@
 package com.project.pokemon.Service;
 
+import com.project.pokemon.model.dto.SignupDto;
+import com.project.pokemon.model.entity.Users;
+import com.project.pokemon.model.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void registerUSer(SignupDto dto) {
+        String email = dto.getEmail();
+        String nickname = dto.getNickname();
+        String password = dto.getPassword();
+
+        Users user = new Users(email, nickname, password);
+        userRepository.save(user);
+    }
+
 }
