73b54cbc96ac4ead3f4f4b0958fa5c1c89c2ada8
MinjuKim <kpoong02@naver.com>
2022-06-11 16:19:47 +0900

    signupdto / uservalidator setting

--- src/main/java/com/project/pokemon/model/dto/SignupDto.java
+++ src/main/java/com/project/pokemon/model/dto/SignupDto.java
@@ -1,4 +1,18 @@
 package com.project.pokemon.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+
+@AllArgsConstructor
+@Setter
+@Getter
 public class SignupDto {
+
+    private String email;
+    private String username;
+    private String password;
+    
 }
--- src/main/java/com/project/pokemon/validator/UserValidator.java
+++ src/main/java/com/project/pokemon/validator/UserValidator.java
@@ -1,4 +1,12 @@
 package com.project.pokemon.validator;
 
+import com.project.pokemon.model.dto.SignupDto;
+
+import java.util.regex.Pattern;
+
 public class UserValidator {
+
+    public static void validaterUsersInput(SignupDto dto) {
+
+    }
 }
