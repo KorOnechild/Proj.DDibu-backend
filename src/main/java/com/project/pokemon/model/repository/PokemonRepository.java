ac9b759f2b9ceb3b617dd9128c43fd5cdac9436e
SeungchanKKK <eccsck@gmail.com>
2022-06-12 01:38:09 +0900

    포켓몬컨트롤러 : 메인페이지, 디테일페이지 구현 포켓몬 리파지토리: 불러오기 설정 포켓몬 엔티티: 엔티티 테이블설정(디스크립션 제외)

--- src/main/java/com/project/pokemon/model/repository/PokemonRepository.java
+++ src/main/java/com/project/pokemon/model/repository/PokemonRepository.java
@@ -1,4 +1,12 @@
 package com.project.pokemon.model.repository;
 
+import com.project.pokemon.model.entity.Pokemon;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface PokemonRepository {
+    List<Pokemon> findAllByOrderByID();
+
+    <T> Optional<T> findById(Long id);
 }
