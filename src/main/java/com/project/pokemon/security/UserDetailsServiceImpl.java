95f5de6d24a670c5fa9249c7c81bb427a94812d7
MinjuKim <kpoong02@naver.com>
2022-06-11 23:29:28 +0900

    Security 기능 구현 / 수정 필요한 부분 - todo

--- src/main/java/com/project/pokemon/security/UserDetailsServiceImpl.java
+++ src/main/java/com/project/pokemon/security/UserDetailsServiceImpl.java
@@ -1,4 +1,29 @@
 package com.project.pokemon.security;
 
-public class UserDetailsServiceImpl {
+import com.project.pokemon.model.entity.Users;
+import com.project.pokemon.model.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+    public UserDetails loadUserByUsername(String nickname) throws UsernameNotFoundException {
+        Users user = userRepository.findByNickname(nickname).orElseThrow(
+                () -> new UsernameNotFoundException("Can't find " + nickname)
+        );
+
+        return new UserDetailsImpl(user);
+    }
 }
--- src/main/java/com/project/pokemon/security/WebSecurityConfig.java
+++ src/main/java/com/project/pokemon/security/WebSecurityConfig.java
@@ -1,4 +1,60 @@
 package com.project.pokemon.security;
 
-public class WebSecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // TODO : WebSecurityConfigurerAdapter 대신 구현하는 방법 알아내기
+
+    @Bean
+    public BCryptPasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.authorizeRequests()
+                .antMatchers("/images/**").permitAll()
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/user/**").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+
+                .antMatchers("/index/**").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/**").permitAll()
+                .antMatchers("*").permitAll()
+
+                .anyRequest().authenticated()
+                .and()
+                // 로그인 기능
+                .formLogin()
+                // 로그인 view 제공 (GET/user/login)
+                .loginPage("/user/login")
+                // 로그인 처리 후 성공 시 URL
+                .defaultSuccessUrl("/")
+                // 로그인 처리 후 실패 시 URL
+                .failureUrl("/user/login")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/user/logout")
+                .logoutSuccessUrl("/")
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 }
