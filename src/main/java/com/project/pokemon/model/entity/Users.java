0a3fba42c7dcfd329a9bf00b2c32ce74a626ac6e
MinjuKim <kpoong02@naver.com>
2022-06-11 12:45:54 +0900

    user entity setting

--- src/main/java/com/project/pokemon/model/entity/Users.java
+++ src/main/java/com/project/pokemon/model/entity/Users.java
@@ -1,4 +1,27 @@
 package com.project.pokemon.model.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
 public class Users {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
 }
