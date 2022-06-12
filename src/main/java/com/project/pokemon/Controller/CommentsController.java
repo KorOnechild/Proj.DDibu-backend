1741875450b053c4f87ecb05bdccbf318d979a29
KorOnechild <kyuhong99@gmail.com>
2022-06-12 13:25:03 +0900

    댓글 수정 API 변경 댓글 삭제 API 추가

--- src/main/java/com/project/pokemon/Controller/CommentsController.java
+++ src/main/java/com/project/pokemon/Controller/CommentsController.java
@@ -3,7 +3,9 @@ package com.project.pokemon.Controller;
 import com.project.pokemon.Service.CommentsService;
 import com.project.pokemon.model.dto.requestDto.CommentsDto;
 import com.project.pokemon.model.dto.responseDto.CommentsListDto;
+import com.project.pokemon.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,23 +18,30 @@ public class CommentsController {
     private final CommentsService commentsService;
 
     //댓글 작성 API
-    @PostMapping("comment/create")
-    public String createComments(CommentsDto commentsDto){
-        return commentsService.createComments(commentsDto);
+    @PostMapping("comment/{pokemonId}")
+    public String createComments(@PathVariable Long pokemonId,
+                                 CommentsDto commentsDto,
+                                 @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        return commentsService.createComments(pokemonId, commentsDto, userDetails);
     }
 
     //댓글 조회 API
     @GetMapping("/comment/{pokemonId}")
-    public List<CommentsListDto> getComments(@PathVariable Long pokemonId){
+    public List<CommentsListDto> getComments(@PathVariable Long pokemonId) {
         return commentsService.getComments(pokemonId);
     }
 
     //댓글 수정 API
-    @PatchMapping("/comment/update/{commentId}")
-    public String updateComments(@PathVariable Long commentId, String newcomments){
+    @PatchMapping("/comment/{commentId}")
+    public String updateComments(@PathVariable Long commentId, String newcomments) {
         return commentsService.updateComments(commentId, newcomments);
     }
 
     //댓글 삭제 API
+    @DeleteMapping("/comment/{commentId}")
+    public String deleteComments(@PathVariable Long commentId) {
+        return commentsService.deleteComments(commentId);
+    }
 
 }
