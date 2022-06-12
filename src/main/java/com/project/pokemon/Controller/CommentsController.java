33b8b1a1275d7f9d94e58c0561b63c95906a84e4
KorOnechild <kyuhong99@gmail.com>
2022-06-12 12:47:47 +0900

    CommentsController - 댓글 조회/수정 변경

--- src/main/java/com/project/pokemon/Controller/CommentsController.java
+++ src/main/java/com/project/pokemon/Controller/CommentsController.java
@@ -2,11 +2,11 @@ package com.project.pokemon.Controller;
 
 import com.project.pokemon.Service.CommentsService;
 import com.project.pokemon.model.dto.requestDto.CommentsDto;
+import com.project.pokemon.model.dto.responseDto.CommentsListDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -23,12 +23,15 @@ public class CommentsController {
 
     //댓글 조회 API
     @GetMapping("/comment/{pokemonId}")
-    public String getComments(@PathVariable Long pokemonId){
+    public List<CommentsListDto> getComments(@PathVariable Long pokemonId){
         return commentsService.getComments(pokemonId);
     }
 
     //댓글 수정 API
-
+    @PatchMapping("/comment/update/{commentId}")
+    public String updateComments(@PathVariable Long commentId, String newcomments){
+        return commentsService.updateComments(commentId, newcomments);
+    }
 
     //댓글 삭제 API
 
