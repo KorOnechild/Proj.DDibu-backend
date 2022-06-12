e4848b9ea0ec3ba938d3ae730eca1781f40ca2b9
KorOnechild <kyuhong99@gmail.com>
2022-06-12 12:48:02 +0900

    CommentsListDto 추가

--- src/main/java/com/project/pokemon/model/dto/responseDto/CommentsListDto.java
+++ src/main/java/com/project/pokemon/model/dto/responseDto/CommentsListDto.java
@@ -1,4 +1,14 @@
 package com.project.pokemon.model.dto.responseDto;
 
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Setter
 public class CommentsListDto {
+    String comments;
+    String nickname;
+    LocalDateTime createdAt;
 }
