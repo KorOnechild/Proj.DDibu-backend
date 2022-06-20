2c52d3a646ce0065c7c6080dff7e337bc298fbe7
MinjuKim <kpoong02@naver.com>
2022-06-20 12:02:29 +0900

    파일위치 변동띠

--- src/main/java/com/project/pokemon/Service/CommentsService.java
+++ src/main/java/com/project/pokemon/Service/CommentsService.java
@@ -1,4 +1,4 @@
-package com.project.pokemon.Controller.Service;
+package com.project.pokemon.Service;
 
 import com.project.pokemon.model.TokenDecode;
 import com.project.pokemon.model.dto.requestDto.CommentsDto;
